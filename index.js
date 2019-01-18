e94a6ce6c2bfe1583df06890f4bcfb432a1869c3
unknown <rehansaeed.mashkraft@gmail.com>
2019-01-18 18:59:29 +0500

    https service added and twillio logic added

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 
-
-
 var http = require('http');
 var url = require('url');
 var myFileLib = require('./lib/data');
 var StringDecoder = require('string_decoder').StringDecoder;
 var handlers = require('./lib/handlers')
-var helpers = require('./lib/helpers')
- // Configure the server to respond to all requests with a string
-var server = http.createServer(function(req,res){
+var helpers = require('./lib/helpers');
+var fs = require('fs');
+var https = require('https')
 
-  // Parse the url
+
+// helpers.sendTwilioSms('3026300487','Hello Rehan',err=>{
+//   console.log('thi is the err',err)
+// });
+
+
+
+// var httpServer = http.createServer(function(req,res){
+//   unifiedServer(req,res);
+// });
+
+// // Start the HTTP server
+// httpServer.listen(3000,function(){
+//   console.log('The HTTP server is running on port '+3000);
+// });
+
+// Instantiate the HTTPS server
+var httpsServerOptions = {
+  'key': fs.readFileSync('./https/key.pem'),
+  'cert': fs.readFileSync('./https/cert.pem')
+};
+var httpsServer = https.createServer(httpsServerOptions,function(req,res){
+  unifiedServer(req,res);
+});
+
+// Start the HTTPS server
+httpsServer.listen(5000,function(){
+ console.log('The HTTPS server is running on port '+5000);
+});
+
+
+
+function unifiedServer(req,res){
+   // Parse the url
   var parsedUrl = url.parse(req.url, true);
 
   // Get the path
@@ -30,30 +61,31 @@ var server = http.createServer(function(req,res){
   var buffer = '';
 
   req.on('data',(data)=>{
-  	buffer += decoder.write(data)
+    buffer += decoder.write(data)
   })
   req.on('end',()=>{
-  	buffer += decoder.end();
-  	buffer = helpers.isJson(buffer) ? JSON.parse(buffer) : {} 
-  	console.log('this si the buffer',buffer)
-  	var chosenHandler = router[trimmedPath] ? router[trimmedPath] : router.notFound;
-  	var data = {
-  		headers:req.headers,
-  		path,
-  		body:buffer,
-  		query:parsedUrl.query,
-  		method
-  	}
-  	chosenHandler(data,(status,response)=>{
-  		status = status ? status : 200;
-  		response = response ? response : {}
-  		res.setHeader('Content-Type','application/json');
-  		res.writeHeader(status)
-  		res.end(JSON.stringify(response));
-  	})
+    buffer += decoder.end();
+    buffer = helpers.isJson(buffer) ? JSON.parse(buffer) : {} 
+    console.log('this si the buffer',buffer)
+    var chosenHandler = router[trimmedPath] ? router[trimmedPath] : router.notFound;
+    var data = {
+      headers:req.headers,
+      path,
+      body:buffer,
+      query:parsedUrl.query,
+      method
+    }
+    chosenHandler(data,(status,response)=>{
+      status = status ? status : 200;
+      response = response ? response : {}
+      res.setHeader('Content-Type','application/json');
+      res.writeHeader(status)
+      res.end(JSON.stringify(response));
+    })
   })
   // Send the response
-});
+}
+
 
 
 
@@ -69,8 +101,3 @@ var router = {
 	notFound:handlers.notFound,
 	checks:handlers.checks
 }
-
-// Start the server
-server.listen(5000,function(){
-  console.log('The server is up and running now');
-});
